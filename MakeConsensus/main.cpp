5f9573601eebc9d663c0f38c7abac2b9d4c13fdb
letlampa <josipa.kovacev@gmail.com>
2015-01-14 23:59:53 +0100

    finish

--- MakeConsensus/main.cpp
+++ MakeConsensus/main.cpp
@@ -251,7 +251,6 @@ int main(int argc, char* argv[])
     string oldConsensusLocation="old_consesnsus.txt";
 
     readsStringList=readReads(readsLocation);
-    cout <<" reads";
     unitigs=readLayout(layoutLocation, readsStringList);
 
     consensusFile.open(consensusResultLocation.c_str(), ofstream::out);
